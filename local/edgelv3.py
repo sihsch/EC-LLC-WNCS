ca29f86e95d50415d4da57953b307ceb1d93dd04
Daniel Mtowe <34525735+danielmtowe@users.noreply.github.com>
2023-07-11 14:24:04 +0900

    Update edgelv3.py

--- local/edgelv3.py
+++ local/edgelv3.py
@@ -3,11 +3,8 @@ import sys
 import time
 import traceback
 import cv2
-from imutils.video import VideoStream
 import zmq
 import pickle
-
-
 
 maximum = 13
 Ab = AlphaBot2()
@@ -16,142 +13,115 @@ rawCapture = cv2.VideoCapture(0)
 time.sleep(2)
 rawCapture.set(3, 160)
 rawCapture.set(4, 120)
-deviation = 0
-last_feature = 0
+
 deviation_list = []
-
 command_recv_time_list = []
-data_processing_time_lv1 = []
-data_processing_time_lv2 = []
-data_processing_time_lv3 = []
-transmition_time_list = []
-looptime_list = []
-
-
-#image_serial = []
-#i = 0
+data_processing_time_list = []
+transmission_time_list = []
+loop_time_list = []
 
 def cal_average(time_list):
-    sum_num = 0
-    for t in time_list:
-        sum_num = sum_num + t
-    avg = sum_num / len(time_list)
-    return avg
+    if len(time_list) != 0:
+        return sum(time_list) / len(time_list)
+    else:
+        return 0
 
 context = zmq.Context()
-print("Connecting to server…")
+print("Connecting to server...")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://192.168.108.153:5554")
 
-
 try:
     while True:
-
         t = time.time()
         t2 = time.time()
         t3 = time.process_time()
 
         ret, image = rawCapture.read()
-        #i  +=1
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray, (5, 5), 0)
         ret, thresh = cv2.threshold(blur, 60, 255, cv2.THRESH_BINARY_INV)
         contours, hierarchy = cv2.findContours(thresh.copy(), 1, cv2.CHAIN_APPROX_NONE)
 
-        # Check to see if any contours is capchaded
         if len(contours) > 0:
             c = max(contours, key=cv2.contourArea)
-            serialized_dict = pickle.dumps(c)
-            processingtime1 = time.time()-t2
-            data_processing_time_lv1.append(processingtime1)
-            
             M = cv2.moments(c)
-            serialized_dict2 = pickle.dumps(M)
+            serialized_dict1 = pickle.dumps(c)
+            processing_time_lv1 = time.time() - t2
+            data_processing_time_lv1.append(processing_time_lv1)
             
-            processingtime2 = time.time()-t2
-            data_processing_time_lv2.append(processingtime2)             
+            serialized_dict2 = pickle.dumps(M)
+            processing_time_lv2 = time.time() - t2
+            data_processing_time_lv2.append(processing_time_lv2)
+            
             if M["m00"] != 0:
-                cx  = int(M["m10"] / M["m00"])
+                cx = int(M["m10"] / M["m00"])
                 cy = int(M["m01"] / M["m00"])
             else:
-                # set values as what you need in the situation
                 cx, cy = 160, 120
 
             setpoint = 80
             deviation = cx - setpoint
             deviation_list.append(abs(deviation))
 
-            cxxx= str(cx)
-            cxx =cxxx.encode('utf-8')
+            cx_str = str(cx)
+            cx_bytes = cx_str.encode('utf-8')
 
-            #size = sys.getsizeof(cxx)
-            #print (size)
-            processingtime = time.time()-t2
-            data_processing_time_lv3.append(processingtime)
+            processing_time_lv3 = time.time() - t2
+            data_processing_time_lv3.append(processing_time_lv3)
 
-            socket.send(cxx)
+            socket.send(cx_bytes)
             reply_from_server = socket.recv()
 
-            command_recv_time = time.time()-t2
+            command_recv_time = time.time() - t2
             outsidetime = time.process_time() - t3
-            transmition_time = command_recv_time - outsidetime
-            transmition_time_list.append(transmition_time)
+            transmission_time = command_recv_time - outsidetime
+            transmission_time_list.append(transmission_time)
             command_recv_time_list.append(command_recv_time)
 
-
-            reply_as_string = reply_from_server.decode('utf-8')  # decode from bytes to Python 3 string
+            reply_as_string = reply_from_server.decode('utf-8')
             power_difference = float(reply_as_string)
 
             Ab.forward()
-            if (power_difference > maximum):
-                power_difference = maximum
-            if (power_difference < - maximum):
-                power_difference = - maximum
-            # Manoeuvring the alphabot
-            if (power_difference < 0):
-                #print("turn left")
-                Ab.setPWMA(maximum + power_difference )
+            power_difference = max(min(power_difference, maximum), -maximum)
+
+            if power_difference < 0:
+                Ab.setPWMA(maximum + power_difference)
                 Ab.setPWMB(maximum)
             else:
-                #print("turn right")
                 Ab.setPWMA(maximum)
-                Ab.setPWMB(maximum - power_difference )
-            #cv2.line(image, (cx, 0), (cx, 720), (255, 0, 0), 1)
-            #cv2.line(image, (0, cy), (1280, cy), (255, 0, 0), 1)
-            #cv2.drawContours(image, contours, -1, (0, 255, 0), 1)
+                Ab.setPWMB(maximum - power_difference)
 
         else:
             d = 80
             deviation_list.append(abs(d))
 
-        looptime = time.time() - t
-        looptime_list.append(looptime)
-        #cv2.putText(image, str(deviation), (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-        #cv2.imshow("path", image)
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-            #break
+        loop_time = time.time() - t
+        loop_time_list.append(loop_time)
+
 except (KeyboardInterrupt, SystemExit):
     Ab.stop()
-    print ("Total number image capture and processed: ", len(deviation_list))
-    print ("The average deviation value is : ",cal_average(deviation_list))   
-    print ("The average time (time.time()) for a single loop execution is: ",cal_average(command_recv_time_list))
-    print ("The average time (time.time()) for command_recv_time is: ",cal_average(command_recv_time_list))
-    print ("The average time (time.time()) for data_processing_time is: ",cal_average(data_processing_time_lv3))
-    print ("Total number of loops the experiment runs (time.time()): ", len(transmition_time_list))
-    print ("The average time (time.time()) for transmition_time is: ",cal_average(transmition_time_list))
-    input ()
-    with open("processing_time_5G.txt", 'a') as f:
-        f.write(str(len(data_processing_time_lv1)) + " processing_time lv1: " +str(cal_average(data_processing_time_lv1))+ " processing_time lv2: " +str(cal_average(data_processing_time_lv2))+" processing_time lv3: " +str(cal_average(data_processing_time_lv3))+ "\n")
-        f.close()
-    with open("edgelv3_5G.txt", 'a') as f:
-        f.write("looptime: " + str(cal_average(looptime_list)) + " command_recv_time: " + str(cal_average(command_recv_time_list)) + " data_processing_time: " +str(cal_average(data_processing_time_lv3)) + " transmition_time: " + str(cal_average(transmition_time_list))+ " deviation: " + str(cal_average(deviation_list))+ "\n")
-        f.close()
-    pass  # Ctrl-C was pressed to end program
+    pass
 except Exception as ex:
     print('Python error with no Exception handler:')
     print('Traceback error:', ex)
     traceback.print_exc()
 finally:
     Ab.stop()
-    #picam.stop()  # stop the camera thread
-    #sys.exit()
+    print("Total number of images captured and processed:", len(deviation_list))
+    print("The average deviation value is:", cal_average(deviation_list))
+    print("The average time for command receive is:", cal_average(command_recv_time_list))
+    print("The average time for data processing (level 1) is:", cal_average(data_processing_time_lv1))
+    print("The average time for data processing (level 2) is:", cal_average(data_processing_time_lv2))
+    print("The average time for data processing (level 3) is:", cal_average(data_processing_time_lv3))
+    print("Total number of loops the experiment runs:", len(transmission_time_list))
+    print("The average time for transmission is:", cal_average(transmission_time_list))
+    input()
+
+    with open("processing_time_5G.txt", 'a') as f:
+        f.write("processing_time_lv1: " + str(cal_average(data_processing_time_lv1)) + " processing_time_lv2: " + str(cal_average(data_processing_time_lv2)) + " processing_time_lv3: " + str(cal_average(data_processing_time_lv3)) + "\n")
+
+    with open("edgelv3_5G.txt", 'a') as f:
+        f.write("looptime: " + str(cal_average(loop_time_list)) + " command_recv_time: " + str(cal_average(command_recv_time_list)) + " data_processing_time: " + str(cal_average(data_processing_time_lv3)) + " transmission_time: " + str(cal_average(transmission_time_list)) + " deviation: " + str(cal_average(deviation_list)) + "\n")
+
+    sys.exit()
